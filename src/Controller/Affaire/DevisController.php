223ca16fefb167d8975e25a68ba27ca08391a37a
Alexandre D <a.domingues@ecole-ipssi.net>
2023-01-24 09:22:48 +0100

    import ouvrage dans lot

--- src/Controller/Affaire/DevisController.php
+++ src/Controller/Affaire/DevisController.php
@@ -132,7 +132,7 @@ class DevisController extends AbstractController
     public function setParent($elements, $el, $parent)
     {
         foreach ($elements as &$element) {
-            dd($parent,$element);
+            //dd($parent,$element);
             if ($element['id'] == $parent['id'] && $element['type'] == $parent['type']) {
                 $element['data'][] = $el;
             } elseif (!empty($element['data'])) {
