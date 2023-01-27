66ff26d163cf73a3ca0916353efac45003f96f4a
bellifa96 <bellifa96@live.fr>
2023-01-27 09:42:58 +0100

    deplacer le css du devis dans webpack

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('devis', './assets/devis.js')
+
     .addEntry('chart', './assets/chart.js')
 
 
