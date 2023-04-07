97693bc4b5c0c582e818f96b9b6878e8142fa7ab
Alexandre D <a.domingues@ecole-ipssi.net>
2023-04-07 15:43:50 +0200

    génération pdf

--- src/Controller/Affaire/DevisController.php
+++ src/Controller/Affaire/DevisController.php
@@ -21,6 +21,7 @@ use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 use Twig\Error\LoaderError;
@@ -40,7 +41,7 @@ class DevisController extends AbstractController
     {
         $this->environment = $environment;
         $this->em = $entityManagerInterface;
-        $this->pdf = $pdfService;
+        $this->pdfService = $pdfService;
     }
 
     #[Route('/', name: 'app_affaire_devis_index', methods: ['GET'])]
@@ -553,5 +554,24 @@ class DevisController extends AbstractController
         }
 
         return $this->redirectToRoute('app_affaire_devis_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/pdf/{id}', name: 'app_affaire_devis_pdf', methods: ['POST', 'GET'])]
+    public function createPdf(Request $request, Devis $devis): Response
+    {
+        $path = "pdf/devis.html";
+        $template = $this->environment->render($path, ['devis' => $devis]);
+        $name = $devis->getTitre();
+
+        $pdf = $this->pdfService->generatePdf($template, $name);
+
+        $response = new Response($pdf);
+        $disposition = $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_INLINE,
+            $name . '.pdf'
+        );
+        $response->headers->set('Content-Disposition', $disposition);
+
+        return $response;
     }
 }
