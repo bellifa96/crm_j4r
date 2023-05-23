423546ce12fc88b9719d08de8402ab4e364ed51d
bellifa96 <bellifa96@live.fr>
2023-05-23 19:33:04 +0200

    import attribut

--- src/Controller/Affaire/AttributOuvrageController.php
+++ src/Controller/Affaire/AttributOuvrageController.php
@@ -21,32 +21,18 @@ class AttributOuvrageController extends AbstractController
     public function index(AttributOuvrageRepository $attributOuvrageRepository): Response
     {
         $data= [
-            ['poids'=>3,'tps'=>-100,'titre'=>"Implantation au sol"],
-            ['poids'=>3,'tps'=>-200,'titre'=>"Implantation en attique"],
-            ['poids'=>5,'tps'=>-600,'titre'=>"Implantation sur toiture zinc"],
-            ['poids'=>5,'tps'=>-600,'titre'=>"Implantation sur toiture tuile"],
-            ['poids'=>3,'tps'=>-300,'titre'=>"Implantation sur étanché"],
-            ['poids'=>3,'tps'=>-200,'titre'=>"Implantation sur passerelle GO"],
-            ['poids'=>2.5,'tps'=>-200,'titre'=>"En passage piéton 1,00 m"],
-            ['poids'=>5,'tps'=>-200,'titre'=>"En passage piéton 1,50 m"],
-            ['poids'=>0,'tps'=>0,'titre'=>"Au R1 à R3 - Standard"],
-            ['poids'=>0,'tps'=>-300,'titre'=>"Au R4 à R6"],
-            ['poids'=>0,'tps'=>-450,'titre'=>"Au R7 à R10"],
-            ['poids'=>0,'tps'=>-600,'titre'=>"Au R10 et +"],
-            ['poids'=>4.5,'tps'=>-300,'titre'=>"En Protection couvreur"],
-            ['poids'=>4.5,'tps'=>-300,'titre'=>"Au droit d'un brisis"],
-            ['poids'=>4.5,'tps'=>-300,'titre'=>"Au droit d'une lucarne"],
-            ['poids'=>5,'tps'=>-300,'titre'=>"Au droit d'une pointe de pignon"],
-            ['poids'=>15,'tps'=>-900,'titre'=>"Départ platine"],
-
-
+            ['poids'=>0,'tps'=>0,'titre'=>"sans console"],
+            ['poids'=>3,'tps'=>0,'titre'=>"Console 28"],
+            ['poids'=>5,'tps'=>0,'titre'=>"Console 39"],
+            ['poids'=>10,'tps'=>0,'titre'=>"Console 73"],
+            ['poids'=>15,'tps'=>0,'titre'=>"Console 109"],
         ];
         foreach($data as $val){
             $attribut = new AttributOuvrage();
             $attribut->setPoidsKG($val['poids']);
             $attribut->setTps($val['tps']);
             $attribut->setTitre($val['titre']);
-            $attribut->setAttributOuvrage($attributOuvrageRepository->find(29));
+            $attribut->setAttributOuvrage($attributOuvrageRepository->find(47));
             $attributOuvrageRepository->save($attribut,true);
         }
         return $this->render('affaire/attribut_ouvrage/index.html.twig', [
