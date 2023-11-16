c1b62a76df648511615587947fac5934c6edc63c
SALAH EDDINE EL MAMOUNI <74799233+msalahe@users.noreply.github.com>
2023-11-16 09:54:44 +0100

    feat : show Dailog

--- src/Controller/Affaire/TypeOuvrageController.php
+++ src/Controller/Affaire/TypeOuvrageController.php
@@ -31,7 +31,6 @@ class TypeOuvrageController extends AbstractController
                 $composantRepository->add($composant);
             }
         }
-        dd($typeOuvrageRepository->findAll()[0]->getAttributOuvrages());
         return $this->render('affaire/type_ouvrage/index.html.twig', [
             'ouvrages' => $ouvrages,
             'composants' => $composants,
