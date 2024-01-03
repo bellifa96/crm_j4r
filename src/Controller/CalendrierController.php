3ab32ea7a9817577fb6adc8fdbc2096f45789ceb
SALAH EDDINE EL MAMOUNI <74799233+msalahe@users.noreply.github.com>
2024-01-03 11:29:05 +0100

    feat : add event

--- src/Controller/CalendrierController.php
+++ src/Controller/CalendrierController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Service\OutlookService;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,13 +17,31 @@ class CalendrierController extends AbstractController
      
     }
 
-
-
     #[Route('/calendrier', name: 'app_calendrier')]
     public function index(): Response
     {
         return $this->render('calendrier/index.html.twig', [
             'controller_name' => 'CalendrierController',
+            'title' => 'Mouvements',
+            'nav' => []
         ]);
+    }
+
+    #[Route('/event-date', name: 'app_event_date')]
+    public function getEventByDate(Request $request): Response
+    {
+        try {
+            $date_du = $request->query->get('datedu');
+            $date_au = $request->query->get('dateau');
+
+            $response = $this->outlookService->getCalendarEvents($date_du, $date_au);
+
+            
+
+            return $this->json($response);
+        } catch (Exception $e) {
+            dd($e->getMessage());
+            return $this->json([]);
+        }
     }
 }
